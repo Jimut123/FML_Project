d70d0fc7c9c0dd757e58115ff7e9204170948a25
Prateek Chanda <prateekkol21@gmail.com>
2022-11-25 10:46:11 +0530

    hugging-face push

--- app_crap.py
+++ app_crap.py
@@ -67,13 +67,29 @@ def movieStatistics(plot_type):
         )
         return fig
     elif (plot_type[0] == "Movie Distribution"):
-        fig = px.pie(movie_df, "release_date", 'itemID')
-        fig.update_layout(
-            title="Movie Distribution",
-            xaxis_title="Movie",
-            yaxis_title="Count",
+        movie_analysis_df = movie_df
+        movie_analysis_df['yearOfRelease'] = movie_df.title.apply(
+        lambda x: int(x[-5:-1]) if x[-5:-1].isdigit() else 1990)
+        df_year_genre = movie_analysis_df.groupby('yearOfRelease')["unknown", "Action", "Adventure", "Animation", "Children's", "Comedy", "Crime",
+                                                            "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"].sum()
+        df_year_genre = df_year_genre.reset_index()
+
+        print(df_year_genre.head())
+        x1 = df_year_genre[-50:-1]  # considering data for last 50 years
+
+        #my_colors = [(x/10.0, x/20.0, 0.75) for x in range(1,19)]
+        cmap = cm.get_cmap('Spectral')
+        ax = px.bar(x1,x='yearOfRelease', y=["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"
+                                        ], title='movies produced every year for each genre')
+        plt.legend(loc=10, bbox_to_anchor=(1.2, .5), ncol=1)
+        #ax.set_xlabel('Year')
+        #ax.set_ylabel('Count of Movies')
+        ax.update_layout(
+        xaxis_title="Year",
+        yaxis_title="Count of Movies",
         )
-        return fig
+        ax.write_image('movie_genre.svg', format='svg')
+        return ax
 
     else:
         raise ValueError("A plot type must be selected")
@@ -167,21 +183,7 @@ with demo:
             cache_examples=True,
         )
         
-    with gr.Box():
-        gr.Markdown(
-                      """
-    ### User Behaviour Statistics
-    #### Select a statistics type to get the distribution.
-    """
-    
-        )     
 
-        gr.Interface(
-                fn=userBehavior,
-                inputs=inputs_user_behavior,
-                outputs=outputs_movie,
-                cache_examples=True,
-            )
 
 if __name__ == "__main__":
     demo.launch()
